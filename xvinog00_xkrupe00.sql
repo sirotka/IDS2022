cff0dda6a900d595a650ddba8f78a598bfe1cafe
sirotka <bogbogenya@gmail.com>
2022-04-03 02:09:52 +0200

    INSERT part

--- xvinog00_xkrupe00.sql
+++ xvinog00_xkrupe00.sql
@@ -43,7 +43,7 @@
 
   CREATE TABLE aircrafts (
     id                NUMBER NOT NULL PRIMARY KEY,
-    producer          VARCHAR(128),
+    type              VARCHAR(128),
     model             VARCHAR(128),
     airline           VARCHAR(2),
 
@@ -98,4 +98,67 @@
     /
 
 
-SET serveroutput ON;
+-- SET serveroutput ON;
+
+    INSERT INTO customers(id, first_name, last_name, email, addr_street, addr_town, addr_post_code, addr_state)
+        VALUES(00001, 'Leonardo', 'Blue', 'leotheleader@gmail.com', 'Bayport Lane', 'New York', 10461, 'USA');
+
+    INSERT INTO customers(id, first_name, last_name, email, addr_street, addr_town, addr_post_code, addr_state)
+        VALUES(00002, 'Raphael', 'Red', 'raphtherebel@gmail.com', 'Cemetery Dr.', 'Waltham', 02453, 'UK');
+
+    INSERT INTO customers(id, first_name, last_name, email, addr_street, addr_town, addr_post_code, addr_state)
+        VALUES(00003, 'Donatello', 'Purple', 'donnythenerd@gmail.com', 'Maintongoon Road', 'Vesper', 3833, 'Australia');
+
+    INSERT INTO customers(id, first_name, last_name, email, addr_street, addr_town, addr_post_code, addr_state)
+        VALUES(00004, 'Michelangelo', 'Orange', 'mickeythejollier@gmail.com', 'MacLaren Street', 'Ottawa', 00157, 'Canada');
+
+    INSERT INTO customers(id, first_name, last_name, email, addr_street, addr_town, addr_post_code, addr_state)
+        VALUES(00005, 'Splinter', 'Master', 'splintertherat@gmail.com', 'Hakulintie', 'Turku', 20210, 'Finland');
+
+--TODO: tickets insert "пиздец лень доделывать эти сраные билеты"
+    INSERT INTO flight_tickets(flight_number, departure_time, arrival_time, aircraft, airline, starting_loc, destination_loc, seat_number, passenger)
+        VALUES('KF8495', TIMESTAMP '2022-04-10 11:00:00.00 +01:00', TIMESTAMP '2022-04-10 15:00:00.00 +03:00', 001, 'LA', 'KDF', 'RTG');
+
+    INSERT INTO flight_tickets(flight_number, departure_time, arrival_time, aircraft, airline, starting_loc, destination_loc, seat_number, passenger)
+        VALUES('KF8495', TIMESTAMP '2022-04-10 11:00:00.00 +01:00', TIMESTAMP '2022-04-10 15:00:00.00 +03:00', 002, 'LA', 'KDF', 'RTG');
+
+    INSERT INTO flight_tickets(flight_number, departure_time, arrival_time, aircraft, airline, starting_loc, destination_loc, seat_number, passenger)
+        VALUES('KF8495', TIMESTAMP '2022-04-10 11:00:00.00 +01:00', TIMESTAMP '2022-04-10 15:00:00.00 +03:00', 003, 'LA', 'KDF', 'RTG');
+
+    INSERT INTO flight_tickets(flight_number, departure_time, arrival_time, aircraft, airline, starting_loc, destination_loc, seat_number, passenger)
+        VALUES('KF8495', TIMESTAMP '2022-04-10 11:00:00.00 +01:00', TIMESTAMP '2022-04-10 15:00:00.00 +03:00', 004, 'LA', 'KDF', 'RTG');
+
+    INSERT INTO flight_tickets(flight_number, departure_time, arrival_time, aircraft, airline, starting_loc, destination_loc, seat_number, passenger)
+        VALUES('KF8495', TIMESTAMP '2022-04-10 11:00:00.00 +01:00', TIMESTAMP '2022-04-10 15:00:00.00 +03:00', 005, 'LA', 'KDF', 'RTG');
+
+
+    INSERT INTO airlines(id, name)
+        VALUES('FA', 'FINLAND AIRLINE');
+
+    INSERT INTO airlines(id, name)
+        VALUES('UA', 'USA AIRLINE');
+
+    INSERT INTO airlines(id, name)
+        VALUES('KA', 'UK AIRLINE');
+
+    INSERT INTO airlines(id, name)
+        VALUES('AA', 'AUSTRALIAN AIRLINE');
+
+    INSERT INTO airlines(id, name)
+        VALUES('CA', 'CANADIAN AIRLINE');
+
+
+    INSERT INTO aircrafts(id, type, model, airline)
+        VALUES(001, 'Boeing', '737 MAX', 'KA');
+
+    INSERT INTO aircrafts(id, type, model, airline)
+        VALUES(002, 'Airbus', 'A3xx', 'UA');
+
+    INSERT INTO aircrafts(id, type, model, airline)
+        VALUES(003, 'Airbus', 'A350', 'CA');
+
+    INSERT INTO aircrafts(id, type, model, airline)
+        VALUES(004, 'Boeing', '200LR', 'UK');
+
+    INSERT INTO aircrafts(id, type, model, airline)
+        VALUES(005, 'Airbus', 'e.g.', 'AA');
